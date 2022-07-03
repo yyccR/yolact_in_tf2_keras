4da9d5d2cf4fadab0eb47086a02cc484d0245240
yang <907616004@qq.com>
2022-07-03 15:42:27 +0800

    修复 loss no gradient 问题.

--- loss.py
+++ loss.py
@@ -34,7 +34,7 @@ class MultiBoxLoss:
     def ohem_conf_loss(self, conf_data, conf_t, pos, batch_size):
         """ 计算分类损失
         :param conf_data: [batch, num_priors, num_classes]
-        :param conf_t: [batch, num_objs]
+        :param conf_t: [batch, num_priors]
         :param pos: [batch, num_priors]
         :param batch_size:
         :return:
@@ -52,31 +52,45 @@ class MultiBoxLoss:
         # Hard Negative Mining
         # loss_c = np.reshape(loss_c, [batch_size, -1])
         # 过滤掉正样本
-        loss_c[pos] = 0
+        pos_indices = tf.where(pos)
+        pos_samples = tf.zeros((pos_indices.get_shape()[0]))
+        loss_c = tf.tensor_scatter_nd_update(loss_c, pos_indices, pos_samples)
+        # loss_c[pos] = 0
         # 过滤掉容易分的负样本
-        loss_c[conf_t < 0] = 0
+        easy_neg_indices = tf.where(conf_t < 0)
+        easy_neg_samples = tf.zeros((easy_neg_indices.get_shape()[0]))
+        loss_c = tf.tensor_scatter_nd_update(loss_c, easy_neg_indices, easy_neg_samples)
+        # loss_c[conf_t < 0] = 0
         # loss_c = -np.sort(-loss_c, axis=1)
 
         # 从大到小排序, 拿到下标索引
-        loss_idx = np.argsort(-loss_c, axis=1)
+        loss_idx = tf.argsort(loss_c, axis=1, direction='DESCENDING')
+        # loss_idx = np.argsort(-loss_c, axis=1)
         # _, loss_idx = loss_c.sort(1, descending=True)
         # 从小到大排序, 拿到[0~num_priors]每个索引对应的loss排序值,比如第一位loss排在18，第二位排到了300,
         # 那么如果只拿前100个负样本计算损失的话，第二位的排名大于100不会被采用
-        idx_rank = np.argsort(loss_idx, axis=1)
+        # idx_rank = np.argsort(loss_idx, axis=1)
+        idx_rank = tf.argsort(loss_idx, axis=1, direction='ASCENDING')
         # _, idx_rank = loss_idx.sort(1)
         # num_pos = pos.long().sum(1, keepdim=True)
-        num_pos = np.sum(pos, axis=1)
+        # num_pos = np.sum(pos, axis=1)
+        num_pos = tf.reduce_sum(pos, axis=1)
 
         # num_neg = torch.clamp(self.ohem_negpos_ratio * num_pos, max=pos.size(1) - 1)
         # 计算负样本个数, 这里负样本为正样本3倍,并且不超过原样本总数
-        num_neg = np.minimum(self.ohem_negpos_ratio * num_pos, pos.shape[1] - 1)
+        # num_neg = np.minimum(self.ohem_negpos_ratio * num_pos, pos.shape[1] - 1)
+        num_neg = tf.minimum((self.ohem_negpos_ratio * num_pos, pos.shape[1] - 1))
         # 先对loss_c从大到小排序, 得到排序下标索引, 再对这个排序下标索引从小到大排序拿到索引
         # neg = idx_rank < num_neg.expand_as(idx_rank)
-        neg = idx_rank < np.broadcast_to(num_neg[:, None], idx_rank.shape)
+        neg = idx_rank < tf.broadcast_to(num_neg[:, None], idx_rank.get_shape())
+        neg = tf.cast(neg, dtype=tf.int32)
+        # neg = idx_rank < np.broadcast_to(num_neg[:, None], idx_rank.shape)
 
         # 去掉那些正样本和容易分的负样本 pos为conf_t>0的，还有conf_t=0和conf_t=-1的，-1的是容易分的负样本
-        neg[pos] = 0
-        neg[conf_t < 0] = 0
+        neg = tf.tensor_scatter_nd_update(neg, pos_indices, pos_samples)
+        # neg[pos] = 0
+        neg = tf.tensor_scatter_nd_update(neg, easy_neg_indices, easy_neg_samples)
+        # neg[conf_t < 0] = 0
 
         # [batch, num_priors] => [batch, num_priors, 1] => [batch, num_priors, num_classes]
         # pos_idx = pos.unsqueeze(2).expand_as(conf_data)
@@ -85,9 +99,11 @@ class MultiBoxLoss:
         # neg_idx = np.broadcast_to(np.expand_dims(neg, axis=-1), conf_data.shape)
 
         # conf_p = conf_data[(pos_idx + neg_idx).gt(0)].view(-1, self.num_classes)
-        conf_p = np.reshape(conf_data[(pos + neg) > 0], (-1, self.num_classes))
+        pos_neg_indices = tf.where((pos+neg) > 0)
+        # conf_p = tf.reshape(conf_data[pos_neg_indices], (-1, self.num_classes))
+        conf_p = np.reshape(conf_data[pos_neg_indices], (-1, self.num_classes))
         # targets_weighted = conf_t[(pos + neg).gt(0)]
-        conf_t = conf_t[(pos + neg) > 0]
+        conf_t = conf_t[pos_neg_indices]
         loss_c = tf.keras.losses.SparseCategoricalCrossentropy()(conf_t, conf_p)
         # loss_c = F.cross_entropy(conf_p, targets_weighted, reduction='none')
         # loss_c = loss_c.sum()
@@ -120,22 +136,22 @@ class MultiBoxLoss:
 
         return self.semantic_segmentation_alpha * loss_s / mask_h / mask_w
 
-    def lincomb_mask_loss(self, pos, idx_t, mask_data, proto_data, gt_masks, prior_gt_boxes):
+    def lincomb_mask_loss(self, pos, idx_t, pred_mask, pred_mask_proto, gt_masks, prior_gt_boxes):
         """ 计算mask损失
 
         :param pos: 正样本mask索引, [batch, num_priors]
         :param idx_t: 每个prior对应的目标边框索引, [batch, num_priors]
-        :param mask_data: [batch, num_priors, 32]
-        :param proto_data: [batch, h/4, w/4, 32]
+        :param pred_mask: [batch, num_priors, 32]
+        :param pred_mask_proto: [batch, h/4, w/4, 32]
         :param gt_masks: [batch, im_h, im_w, num_objs]
         :param prior_gt_boxes: [batch, num_priors, 4(x1,y1,x2,y2)]
         :return:
         """
-        mask_h = proto_data.shape[1]
-        mask_w = proto_data.shape[2]
+        mask_h = pred_mask_proto.shape[1]
+        mask_w = pred_mask_proto.shape[2]
 
         loss_m = 0
-        for idx in range(mask_data.shape[0]):
+        for idx in range(pred_mask.shape[0]):
             # [im_h, im_w, num_objs] => [1/4, 1/4, num_objs]
             downsampled_masks = tf.image.resize(gt_masks[idx], size=(mask_h, mask_w),
                                                 method=tf.image.ResizeMethod.BILINEAR)
@@ -148,18 +164,19 @@ class MultiBoxLoss:
             #     downsampled_masks = downsampled_masks.gt(0.5).float()
 
             cur_pos = pos[idx]
-            pos_idx_t = idx_t[idx, cur_pos]
-            pos_gt_box_t = prior_gt_boxes[idx, cur_pos]
+            pos_idx_t = idx_t[idx][cur_pos]
+            pos_gt_box_t = prior_gt_boxes[idx][cur_pos]
 
             if pos_idx_t.shape[0] == 0:
                 continue
-            proto_masks = proto_data[idx]
-            proto_coef = mask_data[idx, cur_pos, :]
+            proto_masks = pred_mask_proto[idx]
+            proto_coef = pred_mask[idx][cur_pos]
 
             # If we have over the allowed number of masks, select a random sample
             old_num_pos = proto_coef.shape[0]
             if old_num_pos > self.masks_to_train:
-                select = np.random.permutation(proto_coef.shape[0])[:self.masks_to_train]
+                select = tf.random.shuffle(tf.range(proto_coef.shape[0]))[:self.masks_to_train]
+                # select = np.random.permutation(proto_coef.shape[0])[:self.masks_to_train]
                 proto_coef = proto_coef[select]
                 pos_idx_t = pos_idx_t[select]
 
@@ -168,24 +185,28 @@ class MultiBoxLoss:
                 # proto_coef = proto_coef[select, :]
                 # pos_idx_t  = pos_idx_t[select]
 
-                pos_gt_box_t = pos_gt_box_t[select, :]
+                pos_gt_box_t = pos_gt_box_t[select]
 
-            num_pos = proto_coef.shape[0]
-            mask_t = downsampled_masks[:, :, pos_idx_t]
+            # num_pos = proto_coef.shape[0]
+            # [num_selects, mask_h, mask_w] => [mask_h, mask_w, num_selects]
+            mask_t = np.transpose(downsampled_masks[:, :, pos_idx_t],[1,2,0])
 
             # Size: [h, w, 32] x [pos_nums, 32].T = [mask_h, mask_w, num_pos]
-            pred_masks = proto_masks @ proto_coef.transpose()
+            pred_masks = proto_masks @ tf.transpose(proto_coef, [1, 0])
             pred_masks = tf.sigmoid(pred_masks)
             pred_masks = crop(pred_masks, pos_gt_box_t)
-
-            pre_loss = tf.keras.losses.BinaryCrossentropy(reduction=tf.keras.losses.Reduction.NONE)(mask_t, pred_masks)
+            # pre_loss = tf.keras.losses.BinaryCrossentropy(reduction=tf.keras.losses.Reduction.NONE)(mask_t, pred_masks)
+            pre_loss = tf.keras.losses.BinaryCrossentropy()(mask_t, pred_masks)
             # pre_loss = F.binary_cross_entropy(torch.clamp(pred_masks, 0, 1), mask_t, reduction='none')
 
-            pos_gt_csize = np.concatenate(((pos_gt_box_t[..., :2] + pos_gt_box_t[..., 2:]) / 2.,
-                                           pos_gt_box_t[..., 2:] - pos_gt_box_t[..., :2]), axis=1)
-            gt_box_width = pos_gt_csize[..., 2] * mask_w
-            gt_box_height = pos_gt_csize[..., 3] * mask_h
-            pre_loss = tf.reduce_sum(pre_loss) / gt_box_width / gt_box_height
+            # pos_gt_csize = np.concatenate(((pos_gt_box_t[..., :2] + pos_gt_box_t[..., 2:]) / 2.,
+            #                                pos_gt_box_t[..., 2:] - pos_gt_box_t[..., :2]), axis=1)
+            # gt_box_width = pos_gt_csize[..., 2] * mask_w
+            # gt_box_height = pos_gt_csize[..., 3] * mask_h
+
+            # gt_box_width = (pos_gt_box_t[..., 2:] - pos_gt_box_t[..., :2]) * mask_w
+            # gt_box_height = (pos_gt_box_t[..., 2:] - pos_gt_box_t[..., :2]) * mask_h
+            # pre_loss = tf.reduce_sum(pre_loss) / gt_box_width / gt_box_height
 
             # if cfg.mask_proto_normalize_emulate_roi_pooling:
             #     weight = mask_h * mask_w if cfg.mask_proto_crop else 1
@@ -195,12 +216,14 @@ class MultiBoxLoss:
             #     pre_loss = pre_loss.sum(dim=(0, 1)) / gt_box_width / gt_box_height * weight
 
             # If the number of masks were limited scale the loss accordingly
-            if old_num_pos > num_pos:
-                pre_loss *= old_num_pos / num_pos
+            # if old_num_pos > num_pos:
+            #     pre_loss *= old_num_pos / num_pos
+            #
+            # loss_m += tf.reduce_sum(pre_loss)
+            loss_m += pre_loss
 
-            loss_m += tf.reduce_sum(pre_loss)
-
-        losses = loss_m * self.mask_alpha / mask_h / mask_w
+        # losses = loss_m * self.mask_alpha / mask_h / mask_w
+        losses = loss_m * self.mask_alpha
         return losses
 
     def __call__(self, inputs, gt_boxes, gt_labels, gt_masks, *args, **kwargs):
